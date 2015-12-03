50d12a0312e6bf871e03886df4034a5283914f9c
Berat Hoxha <berathoxha4@gmail.com>
2015-12-03 13:32:10 +0100

    'updated'

--- lib/qwerty/datatype.rb
+++ lib/qwerty/datatype.rb
@@ -7,57 +7,31 @@ module Qwerty
       class Datatype < Ruote::Participant
         attr_accessor :collection, :text, :random_num
 
-
         def on_workitem
           language = workitem.fields['text']['quran_simple']['languageList']
            text = Hash.new
           language.each_key do |key| 
-            text[key] = read_file(language[key]['dir'])
+            text = read_file(language[key]['dir'], key)
           end
           workitem.fields['text']['quran_simple'].merge!(text)
           reply
         end 
 
-        def read_file(file_name)
+        def read_file(file_name, key)
           @collection = Array.new
           File.read(file_name).each_line do |line|
             line.gsub!(/\n/, '')
             surah, ayah, verse = line.split(/\|/)
             @collection << { surah: surah, ayah: ayah, verse: verse}
           end
-             random_generate
+             random_generate(key)
         end
 
-        def next_relation(random_number)
-          x = random_number + 1
-          text_verse = text[:verse]
-          next_verse = collection[x]
-          while text_verse[-2, 2] !~ (/[\.\?\!]/)
-            @text[:verse] = text[:verse] + next_verse[:verse]
-            text[:ayah] = "#{text[:ayah]}, #{next_verse[:ayah]}"
-            text_verse = collection[x][:verse]
-            x += 1
-            next_verse = collection[x]
-          end
-        end
-
-        def previous_relation(random_number)
-          x = random_number - 1
-          previous_verse = collection[x]
-          while previous_verse[:verse][-2, 2] !~ (/[\.\?\!]/)
-            text[:verse] = previous_verse[:verse] + text[:verse]
-            x -= 1
-            text[:surah] = text[:surah]
-            text[:ayah] = "#{previous_verse[:ayah]}, #{text[:ayah]}"
-            previous_verse = collection[x]
-          end
-        end
-
-        def random_generate
+        def random_generate(key)
             @random_num = rand(collection.size) if @text == nil
             @text = collection[random_num]
-            previous_relation(random_num)
-            next_relation(random_num)
+            (@verse ||= Hash.new).merge!({ key => text[:verse] })
+            text[:verse] = @verse
             collection.delete_at(random_num)
             return text
         end
