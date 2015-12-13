ced5a2e9f38052b8dfbed8b1cc1850ff0b140641
Korab Hoxha <korab-h@hotmail.com>
2015-12-13 22:58:49 +0100

    Latent Dirichlet Allocation

---
+++ lib/qwerty/classifier/lda.rb
@@ -0,0 +1,27 @@
+require 'lda-ruby'
+
+module Qwerty
+  class Classifier
+    class Lda < Ruote::Participant
+      def on_workitem
+        text = workitem.lookup('source.text')
+        workitem.fields['classifier']['lda'] = {
+          :lda => lda(text)
+        }
+        reply
+      end
+
+      def lda(text)
+        corpus.add_document(::Lda::TextDocument.new(corpus, text))
+        lda = ::Lda::Lda.new(corpus)
+        lda.num_topics = (3)
+        lda.em('random')
+        lda.top_words(3)
+      end
+
+      def corpus
+        @corpus ||= ::Lda::Corpus.new
+      end
+    end
+  end
+end
