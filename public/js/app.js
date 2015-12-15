541e250dc492e75c00482feee01626f7d50350b4
Korab Hoxha <korab-h@hotmail.com>
2015-12-15 01:24:47 +0100

    AMETHYST flat color

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   $(".js-example-basic-single").select2({
-  	tags: true,
-  	tokenSeparators: [',', ' ']
+    tags: true,
+    tokenSeparators: [',', ' ']
   });
 });
