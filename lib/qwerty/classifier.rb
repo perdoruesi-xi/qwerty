30c8824e0c6a4feb78daf5954788681703250ec0
Korab Hoxha <korab-h@hotmail.com>
2016-02-08 11:14:34 +0100

    Classify from en_sahih translation

--- lib/qwerty/classifier.rb
+++ lib/qwerty/classifier.rb
@@ -2,9 +2,7 @@ module Qwerty
   class Classifier < Ruote::Participant
     def on_workitem
       workitem.fields['classifier'] = {
-        :text => "Allah is Subtle with His servants; He gives provisions to whom He wills. And He is the Powerful, the Exalted in Might.
-
-        " # workitem.lookup("text.quran.verse.en_sahih")
+        :text => workitem.lookup("text.quran.verse.en_sahih")
       }
       reply
     end
