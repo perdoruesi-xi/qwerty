7a783229192a1100dc24d08a313be5f1b24361b9
Korab Hoxha <korab-h@hotmail.com>
2016-02-08 11:02:47 +0100

    Parse local trans file

--- lib/qwerty/text/quran.rb
+++ lib/qwerty/text/quran.rb
@@ -1,104 +1,87 @@
 require "./lib/qwerty/text"
-require "./lib/qwerty/json_serializer"
 
 module Qwerty
   class Text
     class Quran < Ruote::Participant
       def on_workitem
-        process_verse
+        random_ayah
         workitem.fields['text']['quran'] = {
-          :source => trans_source,
-          :language_list => language_list,
-          :surah => surah,
+          :source => "http://tanzil.info",
+          :language_list => translations_list,
+          :surah => surah.to_s,
           :ayah => ayah,
-          :verse => random_ayah
+          :verse => verse
         }
         reply
       end
 
-      def process_verse
-        @quran ||= {}
-        quran_transliterations.each do |key, value|
-          path = value.fetch("dir")
-          data = read_quran_trans(path)
-          @quran[key] = data
-        end
-        @quran
-      end
-
-      def get_row
-        @quran.fetch("en_sahih").shuffle.pop
-      end
-
       def random_ayah
-        find_by_surah_num_and_ayah_num(get_row[:surah], get_row[:ayah])
+        quran_transliterations
+        @quran = translations.fetch("en_sahih").sample
+      end
+
+      def surah
+        @quran.fetch("surah")
+      end
+
+      def ayah
+        @quran.fetch("ayah")
+      end
+
+      def verse
+        find_by_surah_num_and_ayah_num(surah, ayah)
       end
 
       def find_by_surah_num_and_ayah_num(surah, ayah)
         trans_list ||= {}
-        @quran.each do |key, v|
-          binding.pry
-          verse_t = v.detect { |r| r[:surah] == surah && r[:ayah] == ayah }
-          verse_t = verse_t.delete(:verse)
-          trans_list[key] = verse_t
+        translations.each do |k, v|
+          verse_t = v.detect { |r| r["surah"] == surah && r["ayah"] == ayah }
+          verse_t = verse_t.delete("verses")
+          trans_list[k] = verse_t
         end
         trans_list
       end
 
-      def surah
-        get_row[:surah]
-      end
-
-      def ayah
-        get_row[:ayah]
-      end
-
-      def verse
-        get_row[:verse]
-      end
-
       def quran_transliterations
-        if config.respond_to?(:quran)
-          language_list
+        translations_list.map do |key, translation|
+          file_path = translation.fetch("dir")
+          translation_text = read_quran_trans(file_path)
+
+          add_translation(key, translation_text)
         end
       end
 
-      def trans_source
-        language_list.fetch("en_sahih", {})["source"]
+      def add_translation(key, content)
+        translations[key] = content
       end
 
-      def language_list
-        config.quran.fetch("language_list")
+      def translations_list
+        Sinatra::Application.settings.quran["language_list"]
+      end
+
+      def translations
+        @translations ||= Hash.new
       end
 
       private
 
-        def read_quran_trans(local_fname)
-          file_path = "#{local_fname}.json"
-          if trans_file_exists?(file_path)
-            load_from_json(file_path)
-          else
-            generate_quran_trans(local_fname)
-          end
+      def read_quran_trans(filename)
+        local_fname = "#{filename}.json"
+        if exists?(local_fname)
+          load_translation(local_fname)
+        else
+          raise StandardError, "Translation file missing: #{local_fname}"
         end
+      end
 
-        def load_from_json(file_path)
-          data = File.read(file_path)
-          JSON.parse(data)
-        end
+      def load_translation(filename)
+        data = File.read(filename)
+        JSON.parse(data)
+      end
 
-        def generate_quran_trans(file_path)
-          json = Qwerty::JsonSerializer.new
-          json.dump_and_load(file_path)
-        end
-
-        def trans_file_exists?(file_path)
-          File.exists?(file_path)
-        end
-
-        def config
-          @config ||= Sinatra::Application.settings
-        end
+      def exists?(filename)
+        File.exists?(filename)
+      end
     end
   end
 end
